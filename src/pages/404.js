f5aee3aec4b6ca6192aaa7ca83f194d80678b61e
Andrew Sellenrick <sellenrick@gmail.com>
2019-08-27 23:36:08 -0500

    Lost source files... adding back in

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+const IndexPage = () => (
+  <Layout fullMenu>
+    <article id="main">
+      <header>
+        <h2>Oops</h2>
+        <p>I see you sneaking around...</p>
+      </header>
+    </article>
+  </Layout>
+);
+
+export default IndexPage;
