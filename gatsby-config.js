9fa88d15f0d33d8006a21c43a26c248324906b8d
Andrew Sellenrick <sellenrick@gmail.com>
2019-08-28 00:54:45 -0500

    Cleaned up static folder from build issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,16 @@ module.exports = {
 	siteMetadata: {
 		title: config.siteTitle
 	},
-	plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass', 'gatsby-plugin-offline']
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sass',
+		'gatsby-plugin-offline',
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-146569002-1',
+				head: true
+			}
+		}
+	]
 };
